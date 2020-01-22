41edbef456f03e1cee8f693a51dd04706c104401
eric-stephenson <rocthered168@gmail.com>
2020-01-22 12:46:02 -0800

    added files

---
+++ myfunc.cpp
@@ -0,0 +1,18 @@
+/*
+ * myfunc.cpp
+ *
+ *  Created on: Jan 22, 2020
+ *      Author: ericstephens28
+ */
+
+#include "myfunc.h"
+
+myfunc::myfunc() {
+	// TODO Auto-generated constructor stub
+
+}
+
+myfunc::~myfunc() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ myfunc.h
@@ -0,0 +1,17 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Jan 22, 2020
+ *      Author: ericstephens28
+ */
+
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+
+class myfunc {
+public:
+	myfunc();
+	virtual ~myfunc();
+};
+
+#endif /* MYFUNC_H_ */
